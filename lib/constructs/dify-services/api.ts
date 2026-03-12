1efe5a0a47fc084ee61218b00dc863392c85a7cd
t-sato-hkd <t-sato@hokendohjin.co.jp>
2026-03-12 16:47:41 +0900

    fix: WorkerコンテナにS3_USE_AWS_MANAGED_IAMを追加

--- lib/constructs/dify-services/api.ts
+++ lib/constructs/dify-services/api.ts
@@ -217,6 +217,7 @@ export class ApiService extends Construct {
         STORAGE_TYPE: 's3',
         S3_BUCKET_NAME: storageBucket.bucketName,
         S3_REGION: Stack.of(storageBucket).region,
+        S3_USE_AWS_MANAGED_IAM: 'true',
 
         DB_DATABASE: postgres.databaseName,
         // pgvector configurations
